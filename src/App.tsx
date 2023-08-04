cbf2f8ac7f45a7b83eaf133df15789ed623d4199
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-08-03 23:55:04 -0300

    CSV Data Finder Based

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './themes/styles.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className='title'>CSV Data Finder</h1>
+      
+      <div className='containerInput'>
+        <input type='text' placeholder='Enter some feature...'/>
+
+        <button className='buttonSearch'>
+          <FiSearch size={25} color='#FFF'/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>Date</h2>
+
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
