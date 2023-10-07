df7c4598d9666e5c867c5dcd3927bc0abcee7141
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-10-07 05:13:33 -0300

    Basic Front-End Tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
-import { Title, ContainerHome, InputSearch, InputFile, CardGenerator } from './components';
+import { ContainerHome ,InputSearch, InputFile, CardGenerator } from './components';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText("Text");
-  expect(linkElement).toBeInTheDocument();
+test('Renders App without errors', () => {
+    render(<App />);
 });
 
+test('Renders ContainerHome without errors', () => {
+    render(<ContainerHome />);
+});
 
+test('Renders CardGenerator without errors', () => {
+    render(<CardGenerator queryParams="" isFileSelected={false} setIsFileSelected={() => {}} />);
+});
+
+test('Renders InputFile without errors', () => {
+    render(<InputFile onUpload={() => {}} isFileSelected={false} />);
+});
+
+test('Renders InputSearch without errors', () => {
+    render(<InputSearch onChangeQueryParam={() => {}} />);
+});
+
+ 
