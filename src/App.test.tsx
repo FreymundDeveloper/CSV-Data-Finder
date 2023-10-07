b1a0d60727dc273d874d0ead290316d6e1a3d184
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-10-07 05:16:32 -0300

    Adjustment of the Test File Environment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import { ContainerHome ,InputSearch, InputFile, CardGenerator } from './components';
+import { ContainerHome, InputSearch, InputFile, CardGenerator } from './components';
 
 test('Renders App without errors', () => {
     render(<App />);
